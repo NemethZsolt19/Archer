1db03714317e4f01109e9080de882ddc11aa355a
Németh Zsolt <nemethzsolt@ktch.hu>
2025-03-07 15:01:04 +0100

    Vazlat

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BookingsComponent } from './bookings/bookings.component';
+import { NewBookingComponent } from './new-booking/new-booking.component';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PlaygroundComponent } from './playground/playground.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookingsComponent,
+    NewBookingComponent,
+    PlaygroundComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/base.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  url="https://localhost:7038/api/"
+  constructor(private http:HttpClient) { }
+
+  getPlaygroundd(){
+    return this.http.get(this.url+"Playgroundd")
+  }
+
+  addBooking(booking:any) {
+    return this.http.post(this.url+"Bookings", booking)
+  }
+}
