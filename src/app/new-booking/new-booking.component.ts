67ca09a7525a1f1a3fe6cfe55bf7aa10168635e4
Németh Zsolt <nemethzsolt@ktch.hu>
2025-03-13 15:09:41 +0100

    Palya-es-foglal

--- src/app/new-booking/new-booking.component.ts
+++ src/app/new-booking/new-booking.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { NgbCalendar, NgbDate } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCalendar, NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-new-booking',
@@ -8,41 +8,11 @@ import { NgbCalendar, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 })
 export class NewBookingComponent {
 
-  calendar = inject(NgbCalendar);
+	today = inject(NgbCalendar).getToday();
 
-	hoveredDate: NgbDate | null = null;
-	fromDate: NgbDate = this.calendar.getToday();
-	toDate: NgbDate | null = this.calendar.getNext(this.fromDate, 'd', 10);
-
-	onDateSelection(date: NgbDate) {
-		if (!this.fromDate && !this.toDate) {
-			this.fromDate = date;
-		} else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-			this.toDate = date;
-		} else {
-			this.toDate = null;
-			this.fromDate = date;
-		}
-	}
-
-	isHovered(date: NgbDate) {
-		return (
-			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
-		);
-	}
-
-	isInside(date: NgbDate) {
-		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-	}
-
-	isRange(date: NgbDate) {
-		return (
-			date.equals(this.fromDate) ||
-			(this.toDate && date.equals(this.toDate)) ||
-			this.isInside(date) ||
-			this.isHovered(date)
-		);
-	}
+	model!: NgbDateStruct;
+	date!: { year: number; month: number; };
+	
 
 
 }
