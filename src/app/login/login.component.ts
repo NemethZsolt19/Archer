5518f063e30b660e4ae9e849298e8af5c05eab43
Németh Zsolt <nemethzsolt@ktch.hu>
2025-03-21 15:43:37 +0100

    signin-up-navbar-javit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,27 +10,58 @@ import { AuthService } from '../auth.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
-  authService = inject(AuthService);
-  router = inject(Router);
 
-  onSubmit(){
-    if(this.loginForm.valid){
-      console.log(this.loginForm.value);
-      this.authService.login(this.loginForm.value)
-      .subscribe((data: any) => {
-        if(this.authService.isLoggedIn()){
-          this.router.navigate(['/admin']);
+  email=""
+  password=""
+  emailPassError=false
+  emailPassMessage=""
+
+  constructor(private auth:AuthService, private router:Router){}
+
+  signIn(){
+    console.log(this.email,"; ",this.password)
+    this.auth.signInMailPassword(this.email, this.password).then(
+      ()=>this.router.navigate(["spiders"])
+    ).catch(
+      (e: any)=>{
+        if (e.code!=4002){
+          this.emailPassError=true
+          this.emailPassMessage=e
+        }else{
+          this.router.navigate(["spiders"])
         }
-        console.log(data);
-      });
-    }
+
+      }
+    )
+
   }
 
-  protected loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  })
+  forgotPassword(){
+    this.auth.forgotPassword(this.email)
+  }
 }
+
+  // email: string = '';
+  // password: string = '';
+  // authService = inject(AuthService);
+  // router = inject(Router);
+
+  // onSubmit(){
+  //   if(this.loginForm.valid){
+  //     console.log(this.loginForm.value);
+  //     this.authService.login(this.loginForm.value)
+  //     .subscribe((data: any) => {
+  //       if(this.authService.isLoggedIn()){
+  //         this.router.navigate(['/admin']);
+  //       }
+  //       console.log(data);
+  //     });
+  //   }
+  // }
+
+  // protected loginForm = new FormGroup({
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   password: new FormControl('', [Validators.required])
+  // })
+
 
