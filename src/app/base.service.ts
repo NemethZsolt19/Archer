a42465f70caf926cdf454e2cf740dcb71904c809
Németh Zsolt <nemethzsolt@ktch.hu>
2025-03-27 14:55:38 +0100

    booking

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -9,28 +9,19 @@ import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/datab
 export class BaseService {
 
   bookingRef: AngularFireList<any>;
-  url="https://localhost:7038/api/"
-  constructor(private db:AngularFireDatabase, private http:HttpClient) {
-    this.bookingRef=db.list('Bookings')
+  // private url="https://localhost:7038/api/"
+  constructor(private db:AngularFireDatabase) {
+    this.bookingRef=db.list('/new-booking')
   }
   //getLoggedUser
 
 
-  getPlaygroundd(){
+
+  pushBooking(booking:any){ 
+    this.bookingRef.push(booking);
+  }
+  getBooking(){
     return this.bookingRef
   }
-
-
-
-  addBooking(booking: any, playgroundd: any, type: any, date: { year: number; month: number; }, p0: any){ 
-    let body = {
-      name:booking.name,
-      playgroundd:booking.playgroundd,
-      type:booking.type,
-      date:booking.date,
-      time:booking.time
-    }
-    this.bookingRef.push(body)
-  }
+  
 }
-
--- src/app/new-booking/new-booking.component.ts
+++ src/app/new-booking/new-booking.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { NgbCalendar, NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { BaseService } from '../base.service';
 import { AuthService } from '../auth.service';
+import {map} from 'rxjs'
+
 
 @Component({
   selector: 'app-new-booking',
@@ -14,26 +16,49 @@ export class NewBookingComponent {
 
 	model!: NgbDateStruct;
 	date!: { year: number; month: number; };
+name: any;
+playgroundd: any;
+type: any;
+time: any;
 	
 
   save() {
     console.log(this.model);
   }
 
-  name:any
-  playgroundd:any
-  type:any
-  year:any
-  month:any
-  time:any
+  booking:any={}
+  newBooking:any={}
+  columns=[
+    {key:"name", label:"Név"},
+    {key:"playgroundd", label:"Pálya"},
+    {key:"type", label:"Játékmód"},
+    {key:"date", label:"Datum"},
+    {key:"time", label:"Idő"}
+  ]
 
   constructor(private base:BaseService, private auth:AuthService){
     
-    this.base.addBooking(this.name, this.playgroundd, this.type, this.date, this.time)  
+    this.base.getBooking().snapshotChanges().pipe(
+      map(
+        (changes) => changes.map(
+          c => ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(
+      (res)=>this.booking=res
+    )
   }
 
   pushBooking(){
-    this.base.addBooking(this.name, this.playgroundd, this.type, this.date, this.time)
+    console.log(this.newBooking, "megy")
+    this.base.pushBooking(this.newBooking)
+    this.newBooking={}
+   
   }
 
+
+
+
 }
+
+
